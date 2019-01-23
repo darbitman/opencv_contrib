d866b9456b3161d8869c53db85233fee096fbc18
darbitman <d.arbitman@gmail.com>
2019-01-22 20:24:42 -0800

    Added tests

--- modules/seamcarver/include/opencv2/seamcarver/imagedimensionstruct.hpp
+++ modules/seamcarver/include/opencv2/seamcarver/imagedimensionstruct.hpp
@@ -1,11 +1,14 @@
 #ifndef OPENCV_SEAMCARVER_IMAGEDIMENSIONSTRUCT_HPP
 #define OPENCV_SEAMCARVER_IMAGEDIMENSIONSTRUCT_HPP
 
-struct CV_EXPORTS ImageDimensionStruct
+namespace cv
 {
-    int32_t NumColumns_ = 0;
-    int32_t NumRows_ = 0;
-    int32_t NumColorChannels_ = 0;
+    struct CV_EXPORTS ImageDimensionStruct
+    {
+        int32_t NumColumns_ = 0;
+        int32_t NumRows_ = 0;
+        int32_t NumColorChannels_ = 0;
+    };
 };
 
 #endif
---
+++ modules/seamcarver/test/test_pixelenergy2d.cpp
@@ -0,0 +1,79 @@
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                        Intel License Agreement
+//                For Open Source Computer Vision Library
+//
+// Copyright (C) 2000, Intel Corporation, all rights reserved.
+// Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//   * Redistribution's of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//   * Redistribution's in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   * The name of Intel Corporation may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the Intel Corporation or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//M*/
+
+#include "test_precomp.hpp"
+
+namespace opencv_test
+{
+    TEST(PixelEnergy2D, DefaultCtor)
+    {
+        double initialMarginEnergy = 390150.0;
+        int32_t initialNumColumns = 1920;
+        int32_t initialNumRows = 1080;
+        int32_t initialNumChannels = 3;
+        cv::PixelEnergy2D pixelEnergyCalculator(initialMarginEnergy);
+        pixelEnergyCalculator.setDimensions(initialNumColumns, initialNumRows, initialNumChannels);
+        cv::ImageDimensionStruct imageDimensions = pixelEnergyCalculator.getDimensions();
+
+        EXPECT_EQ(initialMarginEnergy, pixelEnergyCalculator.getMarginEnergy());
+        EXPECT_EQ(initialNumColumns, imageDimensions.NumColumns_);
+        EXPECT_EQ(initialNumRows, imageDimensions.NumRows_);
+        EXPECT_EQ(initialNumChannels, imageDimensions.NumColorChannels_);
+    }
+
+    TEST(PixelEnergy2D, DimsCtor)
+    {
+        double initialMarginEnergy = 390150.0;
+        int32_t initialNumColumns = 1920;
+        int32_t initialNumRows = 1080;
+        int32_t initialNumChannels = 3;
+        cv::PixelEnergy2D pixelEnergyCalculator(initialNumColumns,
+                                                initialNumRows,
+                                                initialNumChannels,
+                                                initialMarginEnergy);
+        cv::ImageDimensionStruct imageDimensions = pixelEnergyCalculator.getDimensions();
+
+        EXPECT_EQ(initialMarginEnergy, pixelEnergyCalculator.getMarginEnergy());
+        EXPECT_EQ(initialNumColumns, imageDimensions.NumColumns_);
+        EXPECT_EQ(initialNumRows, imageDimensions.NumRows_);
+        EXPECT_EQ(initialNumChannels, imageDimensions.NumColorChannels_);
+    }
+}
