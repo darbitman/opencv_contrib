8f8fcf64ad620f109d79a387c17ffd8f228bd971
Dmitriy <>
2019-07-26 19:37:20 -0700

    Refactored

--- modules/seamcarver/src/computeenergystage.cpp
+++ modules/seamcarver/src/computeenergystage.cpp
@@ -47,33 +47,27 @@
 #include "opencv2/seamcarver/seamcarverstagefactoryregistration.hpp"
 #include "opencv2/seamcarver/verticalseamcarverdata.hpp"
 
-cv::ComputeEnergyStage::ComputeEnergyStage()
-    : bDoRunThread_(false),
-      bThreadIsStopped_(true),
-      bIsInitialized_(false),
-      statusLock_(statusMutex_, std::defer_lock)
-{
-}
+cv::ComputeEnergyStage::ComputeEnergyStage() : bThreadIsRunning_(false), bIsInitialized_(false) {}
 
 cv::ComputeEnergyStage::~ComputeEnergyStage()
 {
     doStopStage();
 
     // clear the queues
-    while (!pInputQueue->empty())
+    while (!pInputQueue_->empty())
     {
-        delete pInputQueue->getNext();
-        pInputQueue->removeNext();
+        delete pInputQueue_->getNext();
+        pInputQueue_->removeNext();
     }
 
-    while (!pOutputQueue->empty())
+    while (!pOutputQueue_->empty())
     {
-        delete pOutputQueue->getNext();
-        pOutputQueue->removeNext();
+        delete pOutputQueue_->getNext();
+        pOutputQueue_->removeNext();
     }
 
     // wait for thread to finish
-    while (bThreadIsStopped_ == false)
+    while (bThreadIsRunning_ == true)
         ;
 }
 
@@ -81,28 +75,34 @@ void cv::ComputeEnergyStage::initialize(cv::Ptr<cv::PipelineQueueData> initData)
 {
     if (bIsInitialized_ == false)
     {
-        cv::PipelineQueueData* data = static_cast<cv::PipelineQueueData*>(initData.get());
+        PipelineQueueData* data = initData.get();
         if (data != nullptr)
         {
-            pipelineStage_ = data->pipeline_stage;
-            pInputQueue = data->p_input_queue;
-            pOutputQueue = data->p_output_queue;
-            bIsInitialized_ = true;
+            pInputQueue_ = data->p_input_queue;
+            pOutputQueue_ = data->p_output_queue;
+
+            if (pInputQueue_ == nullptr || pOutputQueue_ == nullptr)
+            {
+                bIsInitialized_ = false;
+            }
+            else
+            {
+                bIsInitialized_ = true;
+            }
         }
     }
 }
 
 void cv::ComputeEnergyStage::runStage()
 {
-    if (bThreadIsStopped_ && bIsInitialized_)
+    if (bIsInitialized_ && !bThreadIsRunning_)
     {
-        statusLock_.lock();
-        if (bThreadIsStopped_)
+        std::unique_lock<std::mutex> statusLock(statusMutex_);
+        if (!bThreadIsRunning_)
         {
+            statusLock.unlock();
             std::thread(&cv::ComputeEnergyStage::runThread, this).detach();
-            bThreadIsStopped_ = false;
         }
-        statusLock_.unlock();
     }
 }
 
@@ -110,31 +110,33 @@ void cv::ComputeEnergyStage::stopStage() { doStopStage(); }
 
 bool cv::ComputeEnergyStage::isInitialized() const { return bIsInitialized_; }
 
-bool cv::ComputeEnergyStage::isRunning() const { return !bThreadIsStopped_; }
+bool cv::ComputeEnergyStage::isRunning() const { return !bThreadIsRunning_; }
 
 void cv::ComputeEnergyStage::runThread()
 {
-    bDoRunThread_ = true;
+    std::unique_lock<std::mutex> statusLock(statusMutex_);
+    bThreadIsRunning_ = true;
+    statusLock.unlock();
 
-    while (bDoRunThread_)
+    while (bThreadIsRunning_)
     {
-        if (!pInputQueue->empty())
+        if (!pInputQueue_->empty())
         {
             // save the pointer for faster access
-            VerticalSeamCarverData* data = pInputQueue->getNext();
+            VerticalSeamCarverData* data = pInputQueue_->getNext();
 
             calculatePixelEnergy(data->savedImage, data->pixelEnergy);
 
             // move data to next queue
-            pInputQueue->removeNext();
-            pOutputQueue->push(data);
+            pInputQueue_->removeNext();
+            pOutputQueue_->push(data);
         }
     }
 
-    bThreadIsStopped_ = true;
+    bThreadIsRunning_ = true;
 }
 
-void cv::ComputeEnergyStage::doStopStage() { bDoRunThread_ = false; }
+void cv::ComputeEnergyStage::doStopStage() { bThreadIsRunning_ = false; }
 
 void cv::ComputeEnergyStage::calculatePixelEnergy(const cv::Ptr<const cv::Mat>& image,
                                                   std::vector<std::vector<double>>& outPixelEnergy)
